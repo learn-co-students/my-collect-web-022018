568174d4331e85fa474ac971dad202f7a1317cef
lidianvilazio <lidianvilazio@hotmail.com>
2018-02-02 12:03:25 -0500

    My Collect

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect(array)
+    f = []
+    i = 0
+    while i < array.length
+        save = array[i].split(" ")
+        if save.length == 1
+            save.first.upcase!
+        end
+        f << save.first
+        i += 1
+    end
+    f
+end
 
