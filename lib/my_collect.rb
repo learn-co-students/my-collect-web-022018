7f53f0f0708d49c052bc9b1f155c4b53ac6b8faa
lidianvilazio <lidianvilazio@hotmail.com>
2018-02-02 12:30:46 -0500

    My Collect

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,13 +2,23 @@ def my_collect(array)
     f = []
     i = 0
     while i < array.length
-        save = array[i].split(" ")
-        if save.length == 1
-            save.first.upcase!
-        end
-        f << save.first
+        f << yield(array[i])
         i += 1
     end
     f
 end
+
+#def my_collect(array)
+#    f = []
+#    i = 0
+#    while i < array.length
+#        save = array[i].split(" ")
+#        if save.length == 1
+#            save.first.upcase!
+#        end
+#        f << save.first
+#        i += 1
+#    end
+#    f
+#end
 
